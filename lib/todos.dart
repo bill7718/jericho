8b3c744a4042081a66f60411f6ad61ed7562d850
stoke <stokes7718@gmail.com>
2021-08-04 11:28:02 +0100

    hello

--- lib/todos.dart
+++ lib/todos.dart
@@ -4,12 +4,23 @@
 
 /*
 
-1. Create and unit test NewOrganisation
+
 2. Create and unit test ConfirmOrganisation
 3. Create a basic landing page
 4. Create a basic landing page journey
 5. Add to BDD scenario
 6. Create a BDD scenario where the user is invited
+
+7. Unit test Validator
+8. Unit test organisationservice validator
+
+ */
+
+//DONE
+
+/*
+
+1. Create and unit test NewOrganisation
 
 
 
